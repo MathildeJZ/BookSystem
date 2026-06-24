c032d8a958a9fa6281db2e1f30fa32eb381f4a27
Mathilde <mathildezaavi@outlook.dk>
2026-06-24 12:23:49 +0200

    background done

---
+++ BookSystem/frontend/frontpage/frontpage.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontpage',
+  standalone: true,
+  templateUrl: './frontpage.html',
+  styleUrl: './frontpage.css'
+})
+export class Frontpage {}
+
