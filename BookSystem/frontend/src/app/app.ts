168a41fed32fb7a64401a21c34ccf477809710ae
Mathilde <mathildezaavi@outlook.dk>
2026-06-25 10:15:19 +0200

    info-boks done

--- BookSystem/frontend/src/app/app.ts
+++ BookSystem/frontend/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  soldWithOthers = false;
+  amount = 1;
+  otherBooks: string[] = [];
+
+  updateOtherBooks() {
+    this.otherBooks = Array(this.amount).fill('');
+  }
 }
+
+
+
