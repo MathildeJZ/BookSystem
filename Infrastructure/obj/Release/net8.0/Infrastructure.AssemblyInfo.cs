adc6a6a3e45886135297ebdb0bbb33ae638a3ac0
Mathilde <mathildezaavi@outlook.dk>
2026-08-20 18:50:54 +0200

    archichture pic to docs- file

--- Infrastructure/obj/Release/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Release/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde119b3ac50561e610d65a722b399d1a7e1b946")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f25f1811fbaa37258409fbeb2ab9737cb812c18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
